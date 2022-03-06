4ea5daa0b36a958250756e2a14d632308a14977e
manthanank <manthan.ank46@gmail.com>
2022-03-06 22:19:14 +0530

    commit

--- src/Banner.js
+++ src/Banner.js
@@ -4,27 +4,29 @@ import requests from './requests';
 import "./Banner.css"
 
 function Banner() {
-    const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
 
-        async function fetchData() {
-    
-          const request = await axios.get(requests.fetchNetflixOriginals);
-          setMovie(
-            request.data.results[
-            Math.floor(Math.random() * request.data.results.length - 1)
-            ]
-          );
-          // Math.floor(Math.random() * request.data.results.length -1)
-          return request;
-        }
-        fetchData();
-      }, []);
-      
-    function truncate(str, n) {
-        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-      }
+    async function fetchData() {
+
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+        Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      // Math.floor(Math.random() * request.data.results.length -1)
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  // console.log(movie)
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
   return (
     <header className="banner"
@@ -50,6 +52,7 @@ function Banner() {
 
       <div className="banner--fadeBottom" />
     </header>
+
   )
 }
 
