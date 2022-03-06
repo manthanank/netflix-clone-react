ca6d1a8e91e8e33f5e70b35d93409e77278ee8fd
manthanank <manthan.ank46@gmail.com>
2022-03-06 20:40:24 +0530

    commit

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original"
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-
         async function fetchData() {
           const request = await axios.get(fetchUrl);
           setMovies(request.data.results);
