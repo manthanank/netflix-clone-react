14e134707ccec1189af4693389922d28c59a7adb
manthanank <manthan.ank46@gmail.com>
2022-03-06 20:36:10 +0530

    commit

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+
+        async function fetchData() {
+          const request = await axios.get(fetchUrl);
+          setMovies(request.data.results);
+          return request;
+        }
+        fetchData();
+      }, [fetchUrl]);
 
   return (
     <div>
